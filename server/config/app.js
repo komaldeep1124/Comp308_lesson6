9b2ef6c6ea8f7667f6bc420ac731ccec08e0e7e4
komaldeep1124 <kd00441@gmail.com>
2019-03-28 04:22:30 -0400

    created user model

--- server/config/app.js
+++ server/config/app.js
@@ -3,6 +3,12 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+//modules for authentication
+let session=require('express-session');
+let passport=require('passport');
+let passportLocal=require('passport-local');
+let localStrategy=passportLocal.Strategy;
+let flash= require('connect-flash');
 //database setup 
 let mongoose=require('mongoose');
 let DB= require('./db');
@@ -29,6 +35,25 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../node_modules')));
 
+//setup
+app.use(session({
+  secret:"SomeSecret",
+  saveUninitialized:false,
+  resave:false
+    }));
+  //initialize flash
+  app.use(flash());
+  
+  //initialize passport
+  app.use(passport.initialize);
+  app.use(passport.session());
+  //passport user configuration
+  
+  //create user model
+  
+  //implement a user authentication startefy
+  
+  //serialize and deserialize the user info
 app.use('/', indexRouter);
 app.use('/contact-list',contactRouter);
 
